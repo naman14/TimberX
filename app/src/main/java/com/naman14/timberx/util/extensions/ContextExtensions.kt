a26acb41d8df64ce0e20e975e513a8dabd528a69
Aidan Follestad <afollestad@squareup.com>
2019-02-05 11:56:10 -0800

    Make CreatePlaylistDialog translatable

--- app/src/main/java/com/naman14/timberx/util/extensions/ContextExtensions.kt
+++ app/src/main/java/com/naman14/timberx/util/extensions/ContextExtensions.kt
@@ -18,9 +18,25 @@ import android.app.Activity
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.preference.PreferenceManager
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+
+private var toast: Toast? = null
+
+fun Context?.toast(@StringRes message: Int) {
+    if (this == null) {
+        return
+    }
+    toast?.cancel()
+    toast = Toast.makeText(this, message, LENGTH_SHORT)
+            .apply {
+                show()
+            }
+}
 
 fun Context.defaultPrefs() = PreferenceManager.getDefaultSharedPreferences(this)!!
 
