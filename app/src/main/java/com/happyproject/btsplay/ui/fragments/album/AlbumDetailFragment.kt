03046a5ab9c29755558fd289b59db5015a996ec2
Achmad Fachrudin <a2fachrudin@gmail.com>
2020-10-15 10:58:18 +0700

    rename package

---
+++ app/src/main/java/com/happyproject/btsplay/ui/fragments/album/AlbumDetailFragment.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2019 Naman Dwivedi.
+ *
+ * Licensed under the GNU General Public License v3
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+package com.happyproject.btsplay.ui.fragments.album
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.happyproject.btsplay.R
+import com.happyproject.btsplay.constants.Constants.ALBUM
+import com.happyproject.btsplay.databinding.FragmentAlbumDetailBinding
+import com.happyproject.btsplay.extensions.addOnItemClick
+import com.happyproject.btsplay.extensions.argument
+import com.happyproject.btsplay.extensions.filter
+import com.happyproject.btsplay.extensions.getExtraBundle
+import com.happyproject.btsplay.extensions.inflateWithBinding
+import com.happyproject.btsplay.extensions.observe
+import com.happyproject.btsplay.extensions.safeActivity
+import com.happyproject.btsplay.extensions.toSongIds
+import com.happyproject.btsplay.models.Album
+import com.happyproject.btsplay.models.Song
+import com.happyproject.btsplay.ui.adapters.SongsAdapter
+import com.happyproject.btsplay.ui.fragments.base.MediaItemFragment
+import com.happyproject.btsplay.util.AutoClearedValue
+
+class AlbumDetailFragment : MediaItemFragment() {
+    private lateinit var songsAdapter: SongsAdapter
+    lateinit var album: Album
+    var binding by AutoClearedValue<FragmentAlbumDetailBinding>(this)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        album = argument(ALBUM)
+        binding = inflater.inflateWithBinding(R.layout.fragment_album_detail, container)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.album = album
+
+        songsAdapter = SongsAdapter(this).apply {
+            popupMenuListener = mainViewModel.popupMenuListener
+        }
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(safeActivity)
+            adapter = songsAdapter
+            addOnItemClick { position: Int, _: View ->
+                val extras = getExtraBundle(songsAdapter.songs.toSongIds(), album.title)
+                mainViewModel.mediaItemClicked(songsAdapter.songs[position], extras)
+            }
+        }
+
+        mediaItemFragmentViewModel.mediaItems
+                .filter { it.isNotEmpty() }
+                .observe(this) { list ->
+                    @Suppress("UNCHECKED_CAST")
+                    songsAdapter.updateData(list as List<Song>)
+                }
+    }
+}
---
+++ app/src/main/java/com/happyproject/btsplay/ui/fragments/base/MediaItemFragment.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2019 Naman Dwivedi.
+ *
+ * Licensed under the GNU General Public License v3
+ *
+ * This is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+package com.happyproject.btsplay.ui.fragments.base
+
+import android.os.Bundle
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.MEDIA_CALLER
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.MEDIA_ID_ARG
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.MEDIA_TYPE_ARG
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALBUM
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALL_ALBUMS
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALL_ARTISTS
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALL_FOLDERS
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALL_GENRES
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALL_PLAYLISTS
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ALL_SONGS
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_ARTIST
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_GENRE
+import com.happyproject.btsplay.playback.TimberMusicService.Companion.TYPE_PLAYLIST
+import com.happyproject.btsplay.constants.Constants.ACTION_REMOVED_FROM_PLAYLIST
+import com.happyproject.btsplay.constants.Constants.ACTION_SONG_DELETED
+import com.happyproject.btsplay.constants.Constants.ALBUM
+import com.happyproject.btsplay.constants.Constants.ARTIST
+import com.happyproject.btsplay.constants.Constants.CATEGORY_SONG_DATA
+import com.happyproject.btsplay.extensions.argumentOrEmpty
+import com.happyproject.btsplay.extensions.map
+import com.happyproject.btsplay.extensions.observe
+import com.happyproject.btsplay.models.CategorySongData
+import com.happyproject.btsplay.models.Genre
+import com.happyproject.btsplay.models.MediaID
+import com.happyproject.btsplay.models.Playlist
+import com.happyproject.btsplay.ui.fragments.FolderFragment
+import com.happyproject.btsplay.ui.fragments.GenreFragment
+import com.happyproject.btsplay.ui.fragments.PlaylistFragment
+import com.happyproject.btsplay.ui.fragments.album.AlbumDetailFragment
+import com.happyproject.btsplay.ui.fragments.album.AlbumsFragment
+import com.happyproject.btsplay.ui.fragments.artist.ArtistDetailFragment
+import com.happyproject.btsplay.ui.fragments.artist.ArtistFragment
+import com.happyproject.btsplay.ui.fragments.songs.CategorySongsFragment
+import com.happyproject.btsplay.ui.fragments.songs.SongsFragment
+import com.happyproject.btsplay.ui.viewmodels.MediaItemFragmentViewModel
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.core.parameter.parametersOf
+
+open class MediaItemFragment : BaseNowPlayingFragment() {
+
+    protected lateinit var mediaItemFragmentViewModel: MediaItemFragmentViewModel
+
+    private lateinit var mediaType: String
+    private var mediaId: String? = null
+    private var caller: String? = null
+
+    companion object {
+        fun newInstance(mediaId: MediaID): MediaItemFragment {
+            val args = Bundle().apply {
+                putString(MEDIA_TYPE_ARG, mediaId.type)
+                putString(MEDIA_ID_ARG, mediaId.mediaId)
+                putString(MEDIA_CALLER, mediaId.caller)
+            }
+            return when (mediaId.type?.toInt()) {
+                TYPE_ALL_SONGS -> SongsFragment().apply { arguments = args }
+                TYPE_ALL_ALBUMS -> AlbumsFragment().apply { arguments = args }
+                TYPE_ALL_PLAYLISTS -> PlaylistFragment().apply { arguments = args }
+                TYPE_ALL_ARTISTS -> ArtistFragment().apply { arguments = args }
+                TYPE_ALL_FOLDERS -> FolderFragment().apply { arguments = args }
+                TYPE_ALL_GENRES -> GenreFragment().apply { arguments = args }
+                TYPE_ALBUM -> AlbumDetailFragment().apply {
+                    arguments = args.apply { putParcelable(ALBUM, mediaId.mediaItem) }
+                }
+                TYPE_ARTIST -> ArtistDetailFragment().apply {
+                    arguments = args.apply { putParcelable(ARTIST, mediaId.mediaItem) }
+                }
+                TYPE_PLAYLIST -> CategorySongsFragment().apply {
+                    arguments = args.apply {
+                        (mediaId.mediaItem as Playlist).apply {
+                            val data = CategorySongData(name, songCount, TYPE_PLAYLIST, id)
+                            putParcelable(CATEGORY_SONG_DATA, data)
+                        }
+                    }
+                }
+                TYPE_GENRE -> CategorySongsFragment().apply {
+                    arguments = args.apply {
+                        (mediaId.mediaItem as Genre).apply {
+                            val data = CategorySongData(name, songCount, TYPE_GENRE, id)
+                            putParcelable(CATEGORY_SONG_DATA, data)
+                        }
+                    }
+                }
+                else -> SongsFragment().apply {
+                    arguments = args
+                }
+            }
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        mediaType = argumentOrEmpty(MEDIA_TYPE_ARG)
+        mediaId = argumentOrEmpty(MEDIA_ID_ARG)
+        caller = argumentOrEmpty(MEDIA_CALLER)
+
+        val mediaId = MediaID(mediaType, mediaId, caller)
+        mediaItemFragmentViewModel = getViewModel { parametersOf(mediaId) }
+
+        mainViewModel.customAction
+                .map { it.getContentIfNotHandled() }
+                .observe(this) {
+                    when (it) {
+                        ACTION_SONG_DELETED -> mediaItemFragmentViewModel.reloadMediaItems()
+                        ACTION_REMOVED_FROM_PLAYLIST -> mediaItemFragmentViewModel.reloadMediaItems()
+                    }
+                }
+    }
+}
